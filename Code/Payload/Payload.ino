abd0919863e511d14f7eaedc2039c8c400d6b2fa
poppy <jonathanluu122@gmail.com>
2022-01-28 21:31:32 -0500

    update

--- Code/Payload/Payload.ino
+++ Code/Payload/Payload.ino
@@ -4,187 +4,60 @@
  *    Adafruit PT100/P1000 RTD Sensor w/MAX31865 library by Limor Fried/Ladyada @ Adafruit Industries
  * 
  ***************************************************************/
+#include "Queue.h"
+#include "Imu.h"
+#include "Rtd.h"
+#include "Filter.h"
+
 #include "TeensyThreads.h"
 #include "ICM_20948.h"  // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
 #include <Adafruit_MAX31865.h>
 #include <SD.h>
 #include <TimeLib.h>
 
+#define SERIAL_MONITOR false
+#define SD_INSERTED true 
+#define START_RTD true
+#define START_IMU true 
+
+#define IIR_ACC_X_ALPHA 0.1f
+#define IIR_ACC_Y_ALPHA 0.1f
+#define IIR_ACC_Z_ALPHA 0.1f
 
 // SparkFun 9DoF ICM_20948 IMU     
-#define AD0_VAL   1     // The value of the last bit of the I2C address. On the SparkFun 9DoF IMU breakout the default is 1, and when 
 ICM_20948_I2C myICM;    // create an ICM_20948_I2C object
 
 
 // Adafruit_MAX31865 RTD 
-#define RREF      430.0 // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
-#define RNOMINAL  100.0 // 'nominal' 0-degrees-C resistance of the sensor 100.0 for PT100, 1000.0 for PT1000
-#define CS_PIN1 10
-#define CS_PIN2 37
-#define CS_PIN3 36
-
 Adafruit_MAX31865 thermo1 = Adafruit_MAX31865(CS_PIN1);
 Adafruit_MAX31865 thermo2 = Adafruit_MAX31865(CS_PIN2);
 Adafruit_MAX31865 thermo3 = Adafruit_MAX31865(CS_PIN3);
 
 
-// Log Data 
-#define IMU_CSV_NAME    "IMU.csv"
-#define IMU_HEADER_CSV  "DateTime,Scaled_Acc_X_(mg),Scaled_Acc_Y_(mg),Scaled_Acc_Z_(mg),Gyr_X_(DPS),Gyr_Y_(DPS),Gyr_Z_(DPS),Mag_X_(uT),Mag_Y_(uT),Mag_Z_(uT),Tmp_(C)\n"
-#define RTD_CSV_NAME    "RTD.csv"
-#define RTD_HEADER_CSV  "DateTime,RTD1_(C),RTD2_(C),RTD3_(C)\n"
-
-
-// Period for polling sensor data 
-#define RTD_SAMPLE_PERIOD 2000
-#define IMU_SAMPLE_PERIOD 30
 // wrap around after 50 days
 uint32_t RTD_TIME = 0; 
 uint32_t IMU_TIME = 0; 
 
 
-// Queue Size, vars for RTD and IMU data 
-#define MAX_RTD 30
-#define MAX_IMU 30
-
-volatile int FRONT_RTD = 0;
-volatile int REAR_RTD = -1;
-volatile int FRONT_IMU = 0;
-volatile int REAR_IMU = -1;
-
-// mutex for RTD and IMU Queues
-std::mutex m_rtd;
-std::mutex m_imu;
-
-struct rtd
-{
-  String date;   
-  float t1; 
-  float t2;
-  float t3;
-  bool error;
-};
-
-// RTD Queue
-struct rtd QUEUE_RTD[MAX_RTD];
+// queue for polling and logging sensor data 
+Queue_rtd rtd_queue; 
+Queue_imu imu_queue; 
+// Queue_imu imu_queue_fir; 
+Queue_imu imu_queue_iir; 
 
 
-// FIFO First In First Out 
-void enqueue_rtd(struct rtd temps)
-{
-  std::lock_guard<std::mutex> lock(m_rtd); // lock on creation
-  if(REAR_RTD == MAX_RTD-1)
-  {
-      //Serial.println("RTD QUEUE IS FULL!");
-      // wrap around overwrite front of queue
-      FRONT_RTD = 0; 
-      REAR_RTD = -1;
-  }
-
-  // increment rear then assign value to index
-  QUEUE_RTD[++REAR_RTD] = temps; 
-} // unlock at destruction
+/*
+// FIR Filters 
+FIR_Filter fir_acc_x; 
+FIR_Filter fir_acc_y; 
+FIR_Filter fir_acc_z; 
+*/
 
 
-struct rtd dequeue_rtd()
-{ 
-  std::lock_guard<std::mutex> lock(m_rtd); // lock on creation
-  struct rtd temps = {-1,-1,-1,-1,true};
-
-  if(REAR_RTD == -1 || FRONT_RTD > REAR_RTD)
-  {
-      //Serial.println("RTD QUEUE IS EMPTY!");
-      return temps;
-  }
-
-  temps = QUEUE_RTD[FRONT_RTD];
-  FRONT_RTD++;
-  return temps;
-} // unlock at destruction
-
-
-bool is_rtd_queue_empty()
-{
-  std::lock_guard<std::mutex> lock(m_rtd); // lock on creation
-  if(REAR_RTD == -1 || FRONT_RTD > REAR_RTD)
-    return true;
-  else
-    return false; 
-} // unlock at destruction
-
-
-struct imu
-{
-  String date;   
- 
-  // Scaled_Acc
-  float acc_x; 
-  float acc_y;
-  float acc_z;
-  // Gyr
-  float gyr_x;
-  float gyr_y;
-  float gyr_z;
-  // Mag
-  float mag_x;
-  float mag_y;
-  float mag_z;
-  //temp
-  float tmp; 
-  
-  bool error;
-};
-
-// IMU Queue
-struct imu QUEUE_IMU[MAX_IMU];
-
-
-// FIFO First In First Out 
-void enqueue_imu(struct imu sensor)
-{
-  std::lock_guard<std::mutex> lock(m_imu); // lock on creation
-  if(REAR_IMU == MAX_IMU-1)
-  {
-      Serial.println("IMU QUEUE IS FULL!");
-      // wrap around overwrite front of queue
-      FRONT_IMU = 0; 
-      REAR_IMU = -1;
-  }
-
-  // increment rear then assign value to index
-  QUEUE_IMU[++REAR_IMU] = sensor; 
-} // unlock at destruction
-
-
-struct imu dequeue_imu()
-{
-  std::lock_guard<std::mutex> lock(m_imu); // lock on creation
-  struct imu sensor = {-1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,true};
-  if(REAR_IMU == -1 || FRONT_IMU > REAR_IMU)
-  {   
-      m_imu.unlock();
-      Serial.println("IMU QUEUE IS EMPTY!");
-      return sensor;
-  }
-  
-  sensor = QUEUE_IMU[FRONT_IMU];
-  FRONT_IMU++;
-  return sensor;
-} // unlock at destruction
-
-
-bool is_imu_queue_empty()
-{
-  std::lock_guard<std::mutex> lock(m_imu); // lock on creation
-  if(REAR_IMU == -1 || FRONT_IMU > REAR_IMU)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-} // unlock at destruction
+// IIR Filters  
+IIR_Filter iir_acc_x(IIR_ACC_X_ALPHA, 0); 
+IIR_Filter iir_acc_y(IIR_ACC_Y_ALPHA, 0); 
+IIR_Filter iir_acc_z(IIR_ACC_Z_ALPHA, 1000); 
 
 
 bool Init_RTD(bool flag)
@@ -207,6 +80,7 @@ bool Init_Imu(bool flag)
   if (flag)
   {
     // wait for Serial 
+    // remove !Serial code does not run when powered on unless connected to usb 
     while(!Serial)
     {
       Serial.println("Waiting for Serial to initialize..."); 
@@ -239,6 +113,23 @@ bool Init_Imu(bool flag)
   }
   
   return false;
+}
+
+
+void LogToCSV(String dataString, const char* csv_name)
+{
+ // log data 
+ File dataFile = SD.open(csv_name, FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+  dataFile.print(dataString);
+  dataFile.close();
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening file");
+  }
 }
 
 
@@ -277,11 +168,11 @@ void setup()
 {
   Serial.begin(115200);
 
-  bool rtd_is_initialized = Init_RTD(true);
-  bool imu_is_initialized = Init_Imu(true);
+  bool rtd_is_initialized = Init_RTD(START_RTD);
+  bool imu_is_initialized = Init_Imu(START_IMU);
   // wait for 1.5 sec
   delay(1500);
-  Init_Sd_Card(true); 
+  Init_Sd_Card(SD_INSERTED); 
 
   // create thread to poll IMU
   if (imu_is_initialized)
@@ -303,17 +194,17 @@ void setup()
 void loop() 
 {
   // log every "IMU_SAMPLE_PERIOD" ms
-  if (millis() >= IMU_TIME + IMU_SAMPLE_PERIOD)
+  if (millis() >= IMU_TIME + IMU_SAMPLE_PERIOD && SD_INSERTED)
   {
-    Serial.println("  IMULogger");
+    // Serial.println("  IMULogger");
     LogIMU(IMU_CSV_NAME);
     IMU_TIME += IMU_SAMPLE_PERIOD;
   }
 
   // log every "RTD_SAMPLE_PERIOD" ms
-  if (millis() >= RTD_TIME + RTD_SAMPLE_PERIOD)
+  if (millis() >= RTD_TIME + RTD_SAMPLE_PERIOD && SD_INSERTED)
   {
-    Serial.println("  RTDLogger");
+    // Serial.println("  RTDLogger");
     LogRTD(RTD_CSV_NAME);
     RTD_TIME += RTD_SAMPLE_PERIOD;
   }
@@ -325,13 +216,53 @@ void PollIMU()
 
   while(true)
   {
-    Serial.println("PollIMU");
+    // Serial.println("PollIMU");
     if( myICM.dataReady() )
     {
       myICM.getAGMT();                
-      GetAGMTstruct(myICM.agmt);
-      threads.delay(IMU_SAMPLE_PERIOD);
+      String date = TimeStr();
+      // "DateTime,Scaled_Acc_X_(mg),Scaled_Acc_Y_(mg),Scaled_Acc_Z_(mg),Gyr_X_(DPS),Gyr_Y_(DPS),Gyr_Z_(DPS),Mag_X_(uT),Mag_Y_(uT),Mag_Z_(uT),Tmp_(C)\n"
+      float acc_x = myICM.accX();
+      float acc_y = myICM.accY();
+      float acc_z = myICM.accZ();
+      struct imu sensor = {date,
+                          acc_x,                    // Scaled_Acc
+                          acc_y,
+                          acc_z,
+                          iir_acc_x.update(acc_x),  // Scaled_Acc IIR Filtered
+                          iir_acc_y.update(acc_y),
+                          iir_acc_z.update(acc_z),
+                          myICM.gyrX(),         // Gyr
+                          myICM.gyrY(),
+                          myICM.gyrZ(),
+                          myICM.magX(),         // Mag
+                          myICM.magY(),
+                          myICM.magZ(),
+                          myICM.temp()};        // temp
+      imu_queue.enqueue(sensor);
+      
+      // serial plotter 
+      if (SERIAL_MONITOR)
+      {
+        // raw data 
+        Serial.print(sensor.acc_x);
+        Serial.print("\t");
+        Serial.print(sensor.acc_y);
+        Serial.print("\t");
+        Serial.print(sensor.acc_z);
+
+        // filtered data 
+        Serial.print(sensor.acc_iir_x);
+        Serial.print("\t");
+        Serial.print(sensor.acc_iir_y);
+        Serial.print("\t");
+        Serial.println(sensor.acc_iir_z);
+      }
+
+      // threads.delay(IMU_SAMPLE_PERIOD);
+      
     }
+    
     else
     {
       Serial.println("Waiting for data");
@@ -350,180 +281,26 @@ void PollRTD()
   
   while(true)
   {
-    Serial.println("PollRTD");
+    // Serial.println("PollRTD");
     t1 = GetTemp(&thermo1);
     t2 = GetTemp(&thermo2);
     t3 = GetTemp(&thermo3);
     date = TimeStr();
     struct rtd temps = {date,t1,t2,t3,false};
-    enqueue_rtd(temps);
+    rtd_queue.enqueue(temps);
     threads.delay(RTD_SAMPLE_PERIOD);
     threads.yield();
   }
 } 
 
 
-float GetTemp(Adafruit_MAX31865 *thermo)
-{
-  uint16_t rtd = thermo->readRTD();
-  float temperature = thermo->temperature(RNOMINAL, RREF);
-
-  // Serial.print("RTD value: "); Serial.println(rtd);
-  float ratio = rtd;
-  ratio /= 32768;
-  
-  /*
-  Serial.print("Ratio = "); Serial.println(ratio,8);
-  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  Serial.print("Temperature = "); Serial.println(thermo->temperature(RNOMINAL, RREF));
-  Serial.print("RTD: "); 
-  */
-
-  // Check and print any faults
-  /*
-  uint8_t fault = thermo->readFault();
-  if (fault) {
-    Serial.print("Fault 0x"); Serial.println(fault, HEX);
-    if (fault & MAX31865_FAULT_HIGHTHRESH) {
-      Serial.println("RTD High Threshold"); 
-    }
-    if (fault & MAX31865_FAULT_LOWTHRESH) {
-      Serial.println("RTD Low Threshold"); 
-    }
-    if (fault & MAX31865_FAULT_REFINLOW) {
-      Serial.println("REFIN- > 0.85 x Bias"); 
-    }
-    if (fault & MAX31865_FAULT_REFINHIGH) {
-      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
-    }
-    if (fault & MAX31865_FAULT_RTDINLOW) {
-      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
-    }
-    if (fault & MAX31865_FAULT_OVUV) {
-      Serial.println("Under/Over voltage"); 
-    }
-    thermo->clearFault();
-  }
-  Serial.println();
-  */
-  //delay(1000);
-  return temperature;
-}
-
-
-void LogToCSV(String dataString, const char* csv_name)
-{
- // log data 
- File dataFile = SD.open(csv_name, FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-  dataFile.print(dataString);
-  dataFile.close();
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening file");
-  }
-}
-
-
-String LogFormattedFloat(float val, uint8_t leading, uint8_t decimals){
-  float aval = abs(val);
-  String s = "";
-  if(val < 0){
-    s = s + "-";
-  }else{
-    s = s + " ";
-  }
-  for( uint8_t indi = 0; indi < leading; indi++ ){
-    uint32_t tenpow = 0;
-    if( indi < (leading-1) ){
-      tenpow = 1;
-    }
-    for(uint8_t c = 0; c < (leading-1-indi); c++){
-      tenpow *= 10;
-    }
-    if( aval < tenpow){
-      s = s + "0";
-    }else{
-      break;
-    }
-  }
-  if(val < 0){
-    //Serial.print(-val, decimals);
-    s = s + String(-val, decimals);
-  }else{
-    //Serial.print(val, decimals);
-    s = s + String(val, decimals);
-  }
-  return s;
-}
-
-
-String TimeStr()
-{
-  // yyyy-mm-dd hh:mm:ss.ms
-  int num = millis()%1000;
-  String mill;
-  
-  if(num < 10)
-    mill = "0" + String(num);
-  if(num < 100)
-    mill = "0" + String(num);
-  else
-    mill = String(num);
-  
-  String datetime = String(year()) + "-" + String(month()) + "-" + String(day()) + " " + String(hour()) + ":" + String(minute()) + ":" + String(second()) + "." + mill;
-  return datetime;
-}
-
-
-// "DateTime,Scaled_Acc_X_(mg),Scaled_Acc_Y_(mg),Scaled_Acc_Z_(mg),Gyr_X_(DPS),Gyr_Y_(DPS),Gyr_Z_(DPS),Mag_X_(uT),Mag_Y_(uT),Mag_Z_(uT),Tmp_(C)\n"
-void GetAGMTstruct(ICM_20948_AGMT_t agmt)
-{
-  String date = TimeStr();
-  struct imu sensor = {date,myICM.accX(),myICM.accY(),myICM.accZ(),myICM.gyrX(),myICM.gyrY(),myICM.gyrZ(),myICM.magX(),myICM.magY(),myICM.magZ(),myICM.temp()};
-  enqueue_imu(sensor);
-} 
-
-
-void LogRTD(const char *csv_name)
-{
-    struct rtd temps = dequeue_rtd();
-    if(temps.error == false)
-    {
-      // log data 
-      File dataFile = SD.open(csv_name, FILE_WRITE);
-      // if the file is available, write to it:
-      if (dataFile) 
-      {
-        // yyyy-mm-dd hh:mm:ss
-        dataFile.print(temps.date);
-        dataFile.print(",");
-        dataFile.print(temps.t1);
-        dataFile.print(",");
-        dataFile.print(temps.t2);
-        dataFile.print(",");
-        dataFile.print(temps.t3);
-        dataFile.print("\n");
-        dataFile.close();
-  
-        Serial.println("                  Logging RTD !!!");  
-      }
-      // if the file isn't open, pop up an error:
-      else 
-        Serial.println("error opening RTD_CSV_NAME");
-    }
-} 
-
-
 void LogIMU(const char *csv_name)
 {
-  while(!is_imu_queue_empty())
+  while(!imu_queue.empty())
   {
-    //Serial.println("LogIMU while loop");
-    struct imu sensor = dequeue_imu();
+    // Serial.println("LogIMU while loop");
+    // struct imu sensor = dequeue_imu();
+    struct imu sensor = imu_queue.dequeue();
     if(sensor.error == false)
     {
       // log data 
@@ -540,6 +317,13 @@ void LogIMU(const char *csv_name)
         dataFile.print(LogFormattedFloat( sensor.acc_y, 5, 2));
         dataFile.print(",");
         dataFile.print(LogFormattedFloat( sensor.acc_z, 5, 2));
+        dataFile.print(",");
+        // Scaled_Acc IIR Filtered
+        dataFile.print(LogFormattedFloat( sensor.acc_iir_x, 5, 2));
+        dataFile.print(",");
+        dataFile.print(LogFormattedFloat( sensor.acc_iir_y, 5, 2));
+        dataFile.print(",");
+        dataFile.print(LogFormattedFloat( sensor.acc_iir_z, 5, 2));
         dataFile.print(",");
         // gyr(DPS)
         dataFile.print(LogFormattedFloat( sensor.gyr_x, 5, 2));
@@ -560,11 +344,41 @@ void LogIMU(const char *csv_name)
         dataFile.print("\n");
         dataFile.close();
     
-        Serial.println("      Logging IMU!!!");
+        // Serial.println("      Logging IMU!!!");
       }
       // if the file isn't open, pop up an error:
       else 
         Serial.println("error opening IMU_CSV_NAME");
     }
   }
+} 
+
+
+void LogRTD(const char *csv_name)
+{
+    struct rtd temps = rtd_queue.dequeue();
+    if(temps.error == false)
+    {
+      // log data 
+      File dataFile = SD.open(csv_name, FILE_WRITE);
+      // if the file is available, write to it:
+      if (dataFile) 
+      {
+        // yyyy-mm-dd hh:mm:ss
+        dataFile.print(temps.date);
+        dataFile.print(",");
+        dataFile.print(temps.t1);
+        dataFile.print(",");
+        dataFile.print(temps.t2);
+        dataFile.print(",");
+        dataFile.print(temps.t3);
+        dataFile.print("\n");
+        dataFile.close();
+  
+        // Serial.println("                  Logging RTD !!!");  
+      }
+      // if the file isn't open, pop up an error:
+      else 
+        Serial.println("error opening RTD_CSV_NAME");
+    }
 } 
